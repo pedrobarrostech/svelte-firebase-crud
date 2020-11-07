387bd827e20c7457ba65e44bac0339fcd332c2f6
Pedro Barros <pedro.barros@exactaworks.com.br>
2020-11-07 17:20:50 -0300

    First commit

---
+++ src/app.svelte
@@ -0,0 +1,19 @@
+
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./pages/home.svelte";
+  import Orders from "./pages/orders.svelte";
+
+  export let url = "";
+</script>
+
+<Router url="{url}">
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="pedidos">Pedidos</Link>
+  </nav>
+  <div>
+    <Route path="/"><Home /></Route>
+    <Route path="pedidos" component="{Orders}" />
+  </div>
+</Router>
